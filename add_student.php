5f2ce97a26e7ee2b7a53dce079dfc371923ec591
Vu Hoang <thanhhoanghuyinco@gmail.com>
2024-05-25 09:09:06 +0700

    hoan thien not UI

--- add_student.php
+++ add_student.php
@@ -29,32 +29,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<h2>Add Student</h2>
+<div class="container w-50 border rounded shadow-sm p-3 mb-5 bg-white rounded text-dark">
+  <h2 class="text-dark ">Add Student</h2>
+  <form method="post">
+    <div class="form-group">
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" class="form-control" required>
+    </div>
 
-<form method="post">
-  <label for="name">Name:</label><br>
-  <input type="text" id="name" name="name" required><br>
+    <div class="form-group">
+      <label for="class">Class:</label>
+      <select id="class" name="class" class="form-control" required>
+        <?php while ($row = $classes->fetch_assoc()) { ?>
+          <option value="<?php echo $row['MaLop']; ?>"><?php echo $row['TenLop']; ?></option>
+        <?php } ?>
+      </select>
+    </div>
 
-  <label for="class">Class:</label><br>
-  <select id="class" name="class" required>
-    <?php while ($row = $classes->fetch_assoc()) { ?>
-      <option value="<?php echo $row['MaLop']; ?>"><?php echo $row['TenLop']; ?></option>
-    <?php } ?>
-  </select><br>
+    <div class="form-group">
+      <label for="dob">Date of Birth:</label>
+      <input type="date" id="dob" name="dob" class="form-control" required>
+    </div>
 
-  <label for="dob">Date of Birth:</label><br>
-  <input type="date" id="dob" name="dob" required><br>
+    <div class="form-group">
+      <label for="address">Address:</label>
+      <input type="text" id="address" name="address" class="form-control" required>
+    </div>
 
-  <label for="address">Address:</label><br>
-  <input type="text" id="address" name="address" required><br>
+    <div class="form-group">
+      <label for="gender">Gender:</label><br>
+      <input type="radio" id="male" name="gender" value="Male" required>
+      <label for="male">Male</label>
+      <input type="radio" id="female" name="gender" value="Female" required>
+      <label for="female">Female</label>
+    </div>
 
-  <label for="gender">Gender:</label><br>
-  <input type="radio" id="male" name="gender" value="Male" required>
-  <label for="male">Male</label>
-  <input type="radio" id="female" name="gender" value="Female" required>
-  <label for="female">Female</label><br><br>
-
-  <input type="submit" value="Add Student">
-</form>
+    <div class="form-group d-flex justify-content-center">
+      <input type="submit" value="Add Student" class="btn btn-primary">
+    </div>
+  </form>
+</div>
 
 <?php include 'templates/footer.php'; ?>
